b35bf8ccbbec5e42c6d6c4fce50dee3a4e26f0bc
Vaibhav Gupta <vaibhav101098@gmail.com>
2024-05-28 00:14:45 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,21 @@
+"use client";
+
 import MultiSelectInputDropdown from "@/components/MultiSelectInputDropdown";
-import { useState } from "react";
 import { COUNTRIES } from "@/assets/Country";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-3xl mt-12">Multi-Select Dropdown</h1>
       <form action="/api" method="post" className="mt-5">
-        <div>
-          <MultiSelectInputDropdown
-            formFieldName={"countries"}
-            options={COUNTRIES}
-            onChange={(selectedCountries) => {
-              console.debug("selectedCountries", selectedCountries);
-            }}
-            prompt="Select one or more countries"
-          />
-        </div>
+        <MultiSelectInputDropdown
+          formFieldName={"countries"}
+          options={COUNTRIES}
+          onChange={(selectedCountries) => {
+            console.debug("selectedCountries", selectedCountries);
+          }}
+          prompt="Select one or more countries"
+        />
         <input
           type="submit"
           className="bg-blue-500 text-white rounded px-5 py-2 mt-5 cursor-pointer"
--- src/components/MultiSelectInputDropdown.js
+++ src/components/MultiSelectInputDropdown.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
 
 const MultiSelectInputDropdown = ({
   formFieldName,
@@ -8,7 +10,7 @@ const MultiSelectInputDropdown = ({
 }) => {
   const [isJsEnabled, setIsJsEnabled] = useState(false);
   const [selectedOptions, setSelectedOptions] = useState([]);
-  const [optionsListRef] = useState(null);
+  const optionsListRef = useRef(null);
 
   useEffect(() => {
     setIsJsEnabled(true);
@@ -61,7 +63,16 @@ const MultiSelectInputDropdown = ({
   };
 
   return (
-    <div>
+    <label className="relative">
+      <input type="checkbox" className="hidden peer" />
+
+      <div className="cursor-pointer after:content-['▼'] after:text-xs after:ml-1 after:inline-flex after:items-center peer-checked:after:-rotate-180 after:transition-transform inline-flex border rounded px-5 py-2">
+        {prompt}
+        {isJsEnabled && selectedOptions.length > 0 && (
+          <span className="ml-1 text-blue-500">{`(${selectedOptions.length} selected)`}</span>
+        )}
+      </div>
+
       <div className="absolute bg-white border transition-opacity opacity-0 pointer-events-none peer-checked:opacity-100 peer-checked:pointer-events-auto w-full max-h-60 overflow-y-scroll">
         {isJsEnabled && (
           <ul>
@@ -80,7 +91,7 @@ const MultiSelectInputDropdown = ({
                 disabled={!isClearSelectionEnabled}
                 className="w-full text-left px-2 py-1 text-blue-600 disabled:opacity-50"
               >
-                Clear selection
+                Clear All
               </button>
             </li>
           </ul>
@@ -89,7 +100,9 @@ const MultiSelectInputDropdown = ({
           {options.map((option, i) => {
             return (
               <li key={option}>
-                <label className="flex whitespace-nowrap cursor-pointer px-2 py-1 transition-colors hover:bg-blue-100 [&:has(input:checked)]:bg-blue-200">
+                <label
+                  className={`flex whitespace-nowrap cursor-pointer px-2 py-1 transition-colors hover:bg-blue-100 [&:has(input:checked)]:bg-blue-200`}
+                >
                   <input
                     type="checkbox"
                     name={formFieldName}
@@ -104,7 +117,7 @@ const MultiSelectInputDropdown = ({
           })}
         </ul>
       </div>
-    </div>
+    </label>
   );
 };
 
