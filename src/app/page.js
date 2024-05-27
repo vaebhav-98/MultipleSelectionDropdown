05eccc36698d90192da0b68780b5fe67a0bb064c
Vaibhav Gupta <vaibhav101098@gmail.com>
2024-05-27 23:17:17 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
-import Image from "next/image";
+import MultiSelectInputDropdown from "@/components/MultiSelectInputDropdown";
+import { useState } from "react";
+import { COUNTRIES } from "@/assets/Country";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl mt-12">Multi-Select Dropdown</h1>
+      <form action="/api" method="post" className="mt-5">
+        <div>
+          <MultiSelectInputDropdown
+            formFieldName={"countries"}
+            options={COUNTRIES}
+            onChange={(selectedCountries) => {
+              console.debug("selectedCountries", selectedCountries);
+            }}
+            prompt="Select one or more countries"
+          />
+        </div>
+        <input
+          type="submit"
+          className="bg-blue-500 text-white rounded px-5 py-2 mt-5 cursor-pointer"
+        />
+      </form>
     </main>
   );
 }
--- src/components/MultiSelectInputDropdown.js
+++ src/components/MultiSelectInputDropdown.js
@@ -1,11 +1,111 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const MultiSelectInputDropdown = () => {
+const MultiSelectInputDropdown = ({
+  formFieldName,
+  options,
+  onChange,
+  prompt = "Select one or more options",
+}) => {
+  const [isJsEnabled, setIsJsEnabled] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [optionsListRef] = useState(null);
+
+  useEffect(() => {
+    setIsJsEnabled(true);
+  }, []);
+
+  const handleChange = (e) => {
+    const isChecked = e.target.checked;
+    const option = e.target.value;
+
+    const selectedOptionSet = new Set(selectedOptions);
+
+    if (isChecked) {
+      selectedOptionSet.add(option);
+    } else {
+      selectedOptionSet.delete(option);
+    }
+
+    const newSelectedOptions = Array.from(selectedOptionSet);
+
+    setSelectedOptions(newSelectedOptions);
+    onChange(newSelectedOptions);
+  };
+
+  const isSelectAllEnabled = selectedOptions.length < options.length;
+
+  const handleSelectAllClick = (e) => {
+    e.preventDefault();
+
+    const optionsInputs = optionsListRef.current.querySelectorAll("input");
+    optionsInputs.forEach((input) => {
+      input.checked = true;
+    });
+
+    setSelectedOptions([...options]);
+    onChange([...options]);
+  };
+
+  const isClearSelectionEnabled = selectedOptions.length > 0;
+
+  const handleClearSelectionClick = (e) => {
+    e.preventDefault();
+
+    const optionsInputs = optionsListRef.current.querySelectorAll("input");
+    optionsInputs.forEach((input) => {
+      input.checked = false;
+    });
+
+    setSelectedOptions([]);
+    onChange([]);
+  };
+
   return (
     <div>
-      
+      <div className="absolute bg-white border transition-opacity opacity-0 pointer-events-none peer-checked:opacity-100 peer-checked:pointer-events-auto w-full max-h-60 overflow-y-scroll">
+        {isJsEnabled && (
+          <ul>
+            <li>
+              <button
+                onClick={handleSelectAllClick}
+                disabled={!isSelectAllEnabled}
+                className="w-full text-left px-2 py-1 text-blue-600 disabled:opacity-50"
+              >
+                Select All
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={handleClearSelectionClick}
+                disabled={!isClearSelectionEnabled}
+                className="w-full text-left px-2 py-1 text-blue-600 disabled:opacity-50"
+              >
+                Clear selection
+              </button>
+            </li>
+          </ul>
+        )}
+        <ul ref={optionsListRef}>
+          {options.map((option, i) => {
+            return (
+              <li key={option}>
+                <label className="flex whitespace-nowrap cursor-pointer px-2 py-1 transition-colors hover:bg-blue-100 [&:has(input:checked)]:bg-blue-200">
+                  <input
+                    type="checkbox"
+                    name={formFieldName}
+                    value={option}
+                    className="cursor-pointer"
+                    onChange={handleChange}
+                  />
+                  <span className="ml-1">{option}</span>
+                </label>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MultiSelectInputDropdown
+export default MultiSelectInputDropdown;
